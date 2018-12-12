07ea03e7dc0f7b94dff43f055886f6d660a946a6
wanghongchang <wanghongchang@ihomefnt.com>
2018-12-12 20:38:53 +0800

    Hello window

---
+++ MyOpenGL/MyOpenGL/MyOpenGL.cpp
@@ -0,0 +1,18 @@
+// MyOpenGL.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    return 0;
+}
+
